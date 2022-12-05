c9700e4042230f6fada9befaa45b06ebbc72148d
SoumyaD27 <soumyad233@gmail.com>
2022-12-05 15:54:01 +0530

    Player rolls the die to get number between 1 to 6.

---
+++ UC2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & Ladder Game");
+            Console.WriteLine("Player roll the Dice: ");
+            Random random = new Random();
+            int n1 = random.Next(0,7);
+            Console.WriteLine("{0} is the number of steps the player need to move.", n1);
+        }
+    }
+}
