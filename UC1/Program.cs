cf576315ea6cd7a1eca1d9d693eb74b7889defc2
SoumyaD27 <soumyad233@gmail.com>
2022-12-05 15:43:06 +0530

    Single player at 0 positin.

---
+++ UC1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & Ladder Game");
+            Console.WriteLine("First Player roll the Dice: ");
+            Random random = new Random();
+            int n1 = random.Next(7);
+            if (n1==0)
+            {
+                Console.WriteLine("Start");
+            }
+            else
+            {
+                Console.WriteLine("you are not at 0 position");
+            }
+        }
+    }
+}
